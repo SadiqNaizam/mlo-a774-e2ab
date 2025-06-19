cdda06e906914c554e0fa65301c230848cc6c3e9
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 04:10:17 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import DashboardPage from "./pages/DashboardPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import LoginPage from "./pages/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage";
+import SignUpPage from "./pages/RegistrationPage"; // Component is now SignUpPage, file remains RegistrationPage.tsx
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
-
 
 const App = () => (
 <QueryClientProvider client={queryClient}>
@@ -21,16 +19,15 @@ const App = () => (
     <Sonner />
     <BrowserRouter>
         <Routes>
-
-
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<SignUpPage />} /> {/* Default route is Sign Up */}
+          <Route path="/login" element={<LoginPage />} /> {/* Login page is now at /login */}
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
+          {/* Keep original /registration path for SignUpPage if needed, or remove if / is sole entry */}
+          {/* For this conversion, / will be the main sign up page. No separate /registration route. */}
+          
           {/* catch-all */}
           <Route path="*" element={<NotFound />} />
-
-
         </Routes>
     </BrowserRouter>
     </TooltipProvider>
